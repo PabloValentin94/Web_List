5b59175bcbd5b21bcf1dcf04a19140c8d261dd27
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2024-10-27 21:05:04 -0300

    feat: creating the user interface.

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,6 +1,47 @@
+// Class:
+
+class Task
+{
+
+    name;
+    complete;
+
+    constructor()
+    {
+
+        this.name = "";
+        this.complete = false;
+
+    }
+
+}
+
+// Constants and Variables:
+
+var tasks_names = [], task_status = [];
+
 // Functions:
 
+function Add_Task()
+{
 
+
+
+}
+
+function Remove_Task()
+{
+
+
+
+}
+
+function Check_Task()
+{
+
+
+
+}
 
 // Events:
 
