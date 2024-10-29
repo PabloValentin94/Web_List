29d4f6255a262df36add4495231b251064cb2b0a
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2024-10-29 17:03:49 -0300

    refactor: removing the class property 'ID' and adjusting the code to work without it.

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -5,16 +5,14 @@ class Task
 
     // Propriedades.
 
-    id;
     name;
     complete;
 
     // Método construtor (É executado para cada objeto desta classe, no momento em que é criado.).
 
-    constructor(index, description, status)
+    constructor(description, status)
     {
 
-        this.id = index;
         this.name = description;
         this.complete = status;
 
@@ -27,52 +25,6 @@ class Task
 var tasks = []; // Array de controle da página.
 
 // Functions:
-
-function Find_Array_Index(id)
-{
-
-    // Convertendo o parâmetro passado nesta função para um número inteiro decimal.
-
-    id = parseInt(id, 10);
-
-    // Variável de controle da estrutura de repetição a seguir.
-
-    let array_index = 0;
-
-    // Verificando se há mais de um item no Array de controle da página.
-
-    if(tasks.length > 1)
-    {
-
-        /*
-
-            O trecho de código abaixo irá analisar o valor da propriedade ID em todos os objetos presentes no Array de 
-            controle da página e, então, retornar o índice pertencente ao objeto dentro do Array que possua o valor de seu 
-            ID igual ao valor do parâmetro passado nesta função.
-
-        */
-
-        for(let i = 0; i < tasks.length; i++)
-        {
-    
-            if(tasks[i].id === id)
-            {
-    
-                array_index = i;
-    
-                break;
-    
-            }
-    
-        }
-        
-    }
-
-    // Retornando o valor da variável de controle da estrutura de repetição acima.
-
-    return array_index;
-
-}
 
 function Define_List_Events()
 {
@@ -100,7 +52,7 @@ function Define_List_Events()
 
             // Função que define o estado de uma caixa de seleção (Marcada ou desmarcada.).
 
-            Modify_Task_Status(Find_Array_Index(this.parentElement.querySelector(".id").value));
+            Modify_Task_Status(parseInt(this.parentElement.querySelector(".index").value, 10));
 
         }
 
@@ -110,7 +62,7 @@ function Define_List_Events()
 
             // Função que edita uma tarefa.
 
-            Edit_Task(Find_Array_Index(this.parentElement.querySelector(".id").value));
+            Edit_Task(parseInt(this.parentElement.querySelector(".index").value, 10));
 
         }
 
@@ -120,7 +72,7 @@ function Define_List_Events()
 
             // Função que remove uma tarefa.
 
-            Remove_Task(Find_Array_Index(this.parentElement.querySelector(".id").value));
+            Remove_Task(parseInt(this.parentElement.querySelector(".index").value, 10));
 
         }
 
@@ -151,27 +103,28 @@ function Reload_List()
     
         Adicionando as tarefas, uma por uma, na lista de tarefas da página. É preciso fazer isso pois o valor do item 
         salvo no Local Storage foi alterado, o que implica que a listagem precisa ser atualizada, pois podem haver novas 
-        mudanças.
+        alterações.
 
     */
 
-    tasks.forEach(task => {
+    for(let i = 0; i < tasks.length; i++)
+    {
 
         document.getElementById("list").innerHTML += `
             <div class="item">
 
-                <input class="id" type="hidden" value="${task.id}">
+                <input class="index" type="hidden" value="${i}">
 
-                <input class="status" type="checkbox"${(task.complete) ? " checked" : ""}>
+                <input class="status" type="checkbox"${(tasks[i].complete) ? " checked" : ""}>
 
-                <p class="description"> ${(task.complete) ? task.name.strike() : task.name} </p>
+                <p class="description"> ${(tasks[i].complete) ? tasks[i].name.strike() : tasks[i].name} </p>
 
                 <button class="removal"> <i class="bx bxs-trash-alt">  </i> </button>
 
             </div>
         `;
-
-    });
+        
+    }
 
     /*
 
@@ -187,39 +140,9 @@ function Reload_List()
 function Add_Task(task_description)
 {
 
-    // Variável que será usada para definir o ID da próxima tarefa criada.
-
-    let task_id = 0;
-
-    /*
-
-        O trecho de código abaixo obtém o ID de maior valor presente no Array de controle da página e então o salva na variável acima. 
-        Essa variável então tem seu valor aumentado em uma unidade, garantindo que o ID da próxima tarefa criada sempre será o maior 
-        existente dentro do Array de controle.
-
-    */
-
-    if(tasks.length > 0)
-    {
-
-        tasks.forEach(task => {
-
-            if(task.id > task_id)
-            {
-
-                task_id = task.id;
-
-            }
-
-        });
-
-        task_id++;
-
-    }
-
     // Adicionando uma nova tarefa ao Array de controle da página.
 
-    tasks.push(new Task(task_id, task_description, false));
+    tasks.push(new Task(task_description, false));
 
     // Atualizando a lista de tarefas.
 
@@ -230,17 +153,26 @@ function Add_Task(task_description)
 function Edit_Task(index)
 {
 
-    // Salvando o valor atual da tarefa selecionada para que ele possa ser reutilizado, caso o usuário não preencha nada a seguir.
+    // Salvando o nome atual da tarefa selecionada para que ele possa ser reutilizado, se necessário.
 
     const old_name = tasks[index].name;
 
     // Alterando o nome da tarefa selecionada, com base no que o usuário digitou.
 
-    tasks[index].name = prompt("Insert the new name for the selected task:", tasks[index].name);
+    tasks[index].name = prompt("Insert the new name for the selected task:", old_name); 
+
+    /*
+
+        Removendo possíveis espaços em branco ao redor do texto informado. Caso esse texto seja composto apenas por 
+        espaços em branco, o retorno é uma string vazia.
+
+    */
+
+    tasks[index].name = (typeof tasks[index].name === "string") ? tasks[index].name.trim() : null;
 
     // Verificando se o usuário realmente preencheu a propriedade com um valor aceitável.
 
-    if(tasks[index].name === null)
+    if(tasks[index].name === null || tasks[index].name === "")
     {
 
         // Redefinindo o valor da propriedade.
@@ -282,7 +214,7 @@ function Remove_Task(index)
         for(let i = 0; i < tasks.length; i++)
         {
 
-            if(Find_Array_Index(tasks[i].id) !== index)
+            if(i !== index)
             {
     
                 new_list.push(tasks[i]);
@@ -348,9 +280,29 @@ document.querySelector("form").onsubmit = function(event) {
 
     event.preventDefault();
 
-    // Função que adiciona uma tarefa.
+    // Obtendo o valor da variável do formulário.
 
-    Add_Task((new FormData(event.target)).get("task"));
+    const value = (new FormData(event.target)).get("task").trim();
+
+    // Verificando se o valor da variável do formulário não possui apenas espaços em branco.
+
+    if(value !== "")
+    {
+
+        // Função que adiciona uma tarefa.
+
+        Add_Task(value);
+
+    }
+
+    else
+    {
+
+        // Exibindo um aviso ao usuário.
+
+        alert("Insira uma descrição válida para continuar.");
+
+    }
 
     // Limpando o campo do formulário.
 
